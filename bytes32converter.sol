27ea04aae262f0b308c928cb683680891ba3ad81
unidaonetwork <unidaonetwork@gmail.com>
2022-04-20 10:08:43 +0530

    main

--- bytes32converter.sol
+++ bytes32converter.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.4.24;
 contract converter {
     function convertInt(uint256 integer_) public pure  returns(bytes32){
        bytes32  binfo = bytes32(integer_);
-       return binfo;
+       return binfo; 
     }
 }
